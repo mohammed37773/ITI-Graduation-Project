9420ba4ecf75ccbd3b65d18a005c6ad47adf4dd8
hatem-dotnet <hatemmustafamuhammad@gmail.com>
2026-06-07 01:36:36 +0300

    Update core2

--- api/nurseries/NurseriesNetwork.Core/DTOs/Nursery/CreateNurseryDto.cs
+++ api/nurseries/NurseriesNetwork.Core/DTOs/Nursery/CreateNurseryDto.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace NurseriesNetwork.Core.DTOs.Nursery
 {
     public record CreateNurseryDto(
-     string Name,
-     string Description,
-     decimal DailyPrice,
-     int AgeRangeMin,
-     int AgeRangeMax,
-     int Capacity,
-     string Address,
-     string City,
-     string District,
-     double Latitude,
-     double Longitude
- );
+    [Required][MaxLength(200)] string Name,
+    [Required] string Description,
+    [Required][Range(1, 10000)] decimal DailyPrice,
+    [Required][Range(0, 12)] int AgeRangeMin,
+    [Required][Range(1, 12)] int AgeRangeMax,
+    [Required][Range(1, 500)] int Capacity,
+    [Required] string Address,
+    [Required] string City,
+    [Required] string District,
+    [Required][Range(-90, 90)] double Latitude,
+    [Required][Range(-180, 180)] double Longitude
+);
 }
---
+++ api/nurseries/NurseriesNetwork.Core/DTOs/Payment/PaymentResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NurseriesNetwork.Core.DTOs.Payment
+{
+    public record PaymentResponseDto(
+     bool IsSuccess,
+     string TransactionId,
+     string Message,
+     decimal Amount,
+     string Method
+ );
+}
